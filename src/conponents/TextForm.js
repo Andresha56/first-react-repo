3ad270f60eee7575b8cc494bc4225b34026bdcbf
Andresha <lakshshaw570@gmail.com>
2022-12-29 14:06:24 +0530

    added word abd character counter

--- src/conponents/TextForm.js
+++ src/conponents/TextForm.js
@@ -14,18 +14,23 @@ function TextForm() {
        setText(event.target.value)
     
     }
+
+
    
-    let [Text, setText] = useState();
+    let [Text, setText] = useState("");
     return (
-        <div className="textareaCon row">
+        <div className="textareaCon">
             <div className="mb-3 col-6 m-auto">
                 <textarea className="form-control" onChange={handelChange} rows="3" value={Text} placeholder="Enter your Text here"></textarea>
             </div>
             <div className="btn-con">
             <input className="btn btn-success" type="button" value="UpperCase"  onClick={handelSubmit}/>
             </div>
-
-        </div>
+    <h1>Characters={Text.length}</h1>
+    <h1>word={Text.split(" ").length-1  }</h1>
+    
+   
+    </div>
 
 
     )
